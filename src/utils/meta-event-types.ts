974c2179be68da14904e2b337b5f7b189f2b58b7
craciunlabs <claudiubackend@gmail.com>
2025-04-01 12:20:00 -0400

    debug2 added

--- src/utils/meta-event-types.ts
+++ src/utils/meta-event-types.ts
@@ -1,19 +1,67 @@
 // src/utils/meta-event-types.ts
 
-export interface UserData {
-    fbp?: string | null;
-    fbc?: string | null;
-  }
-  
-  export interface CustomData {
-    value: number;
-    currency: string;
-    content_ids?: string[];
-    content_name?: string;
-    [key: string]: any; // This allows arbitrary properties
-  }
-  
-  export interface EventOptions {
-    url?: string;
-    eventId?: string;
-  }
+export interface CustomData {
+  value?: number;
+  currency?: string;
+  content_name?: string;
+  content_category?: string;
+  content_ids?: string[];
+  contents?: Array<{
+      id?: string;
+      quantity?: number;
+      item_price?: number;
+  }>;
+  content_type?: string;
+  order_id?: string;
+  predicted_ltv?: number;
+  num_items?: number;
+  search_string?: string;
+  status?: string;
+  [key: string]: any;
+}
+
+export interface EventOptions {
+  debug?: boolean;
+  eventID?: string;
+  eventSourceUrl?: string;
+  userData?: {
+      email?: string;
+      phone?: string;
+      firstName?: string;
+      lastName?: string;
+      gender?: string;
+      city?: string;
+      state?: string;
+      zip?: string;
+      country?: string;
+      externalId?: string;
+      clientIpAddress?: string;
+      clientUserAgent?: string;
+      fbc?: string;
+      fbp?: string;
+      subscriptionId?: string;
+      fbLoginId?: string;
+      leadId?: string;
+      [key: string]: string | undefined;
+  };
+}
+
+export type EventName = 
+  | 'PageView'
+  | 'AddPaymentInfo'
+  | 'AddToCart'
+  | 'AddToWishlist'
+  | 'CompleteRegistration'
+  | 'Contact'
+  | 'CustomizeProduct'
+  | 'Donate'
+  | 'FindLocation'
+  | 'InitiateCheckout'
+  | 'Lead'
+  | 'Purchase'
+  | 'Schedule'
+  | 'Search'
+  | 'StartTrial'
+  | 'SubmitApplication'
+  | 'Subscribe'
+  | 'ViewContent';
