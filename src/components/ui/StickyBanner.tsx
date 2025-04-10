7d4548f30d60f5cdda5ccd5a2775b93c717f9319
craciunlabs <claudiubackend@gmail.com>
2025-04-10 11:42:15 -0400

    fix: update sticky banner link to waiting list page

--- src/components/ui/StickyBanner.tsx
+++ src/components/ui/StickyBanner.tsx
@@ -1,25 +1,18 @@
 // src/components/ui/StickyBanner.tsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './StickyBanner.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFire } from '@fortawesome/free-solid-svg-icons';
 
 const StickyBanner: React.FC = () => {
-  const handleClick = (e: React.MouseEvent) => {
-    e.preventDefault();
-    const pricingSection = document.getElementById('pricing');
-    if (pricingSection) {
-      pricingSection.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
   return (
-    <a href="#pricing" className={styles.stickyBanner} onClick={handleClick}>
+    <Link to="/waiting-list" className={styles.stickyBanner}>
       <FontAwesomeIcon icon={faFire} className={styles.fireEmoji} />
       SOLD OUT for 2025
       <span className={styles.remainingText}>JOIN WAITING LIST FOR 2026</span>
       <FontAwesomeIcon icon={faFire} className={styles.fireEmoji} />
-    </a>
+    </Link>
   );
 };
 
